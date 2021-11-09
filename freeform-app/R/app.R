ba355cae4483c201d062648393ee9f70944cec0d
Tim Band <t.band@nnnn.com>
2021-11-09 14:40:12 +0000

    Progress bar, status text and SVG plot output

--- freeform-app/R/app.R
+++ freeform-app/R/app.R
@@ -1,4 +1,14 @@
 makePlot <- function(x, y, type, pch, bg) {
+  shinylight::sendInfoText("processing...");
+  shinylight::sendProgress(0, 3);
+  Sys.sleep(0.2);
+  shinylight::sendProgress(1, 3);
+  Sys.sleep(0.1);
+  shinylight::sendInfoText("we're about halfway through...");
+  Sys.sleep(0.1);
+  shinylight::sendProgress(2, 3);
+  Sys.sleep(0.2);
+  shinylight::sendProgress(3, 3);
   plot(x, y, type=type, pch=pch, bg=bg)
   data.frame(
     sum=x+y,
@@ -7,9 +17,14 @@ makePlot <- function(x, y, type, pch, bg) {
 }
 
 freeformServer <- function(port=NULL) {
+  # To use the latest versions of the files within the www directory
   appDir <- R.utils::getAbsolutePath("freeform-app/inst/www")
+  # Or, if your app is a package, to use the files within the package (and
+  # to be able to run it not from the source directory):
+  # appDir <- system.file('www', package='mypackagename')
   shinylight::slServer(host='0.0.0.0', port=port, appDir=appDir, daemonize=TRUE,
     interface=list(
+      # Add as many functions as you need to call from the client side
       makePlot=makePlot
     )
   )
--- js-to-r-app/R/app.R
+++ js-to-r-app/R/app.R
@@ -1,5 +1,9 @@
 js2rServer <- function(port=NULL) {
+  # To use the latest versions of the files within the www directory
   appDir <- R.utils::getAbsolutePath("js-to-r-app/inst/www")
+  # Or, if your app is a package, to use the files within the package (and
+  # to be able to run it not from the source directory):
+  # appDir <- system.file('www', package='mypackagename')
   shinylight::slRunRServer(host='0.0.0.0', port=port, appDir=appDir, daemonize=TRUE,
     permittedSymbols=c(
       '<-', 'c', 'plot', 'data.frame', '+', '-', '*', '/', '^',
